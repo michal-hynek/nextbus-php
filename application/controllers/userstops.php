491c5bd1f760183f5b759248ed3349092ff47521
COLIN BUCKTON <bbuckton@bcit.ca>
2013-03-23 13:28:48 -0700

    saving work on userstops to update arrivals from Michal

--- application/controllers/userstops.php
+++ application/controllers/userstops.php
@@ -4,7 +4,17 @@ require_once('restricted.php');
 
 class UserStops extends Restricted {
 
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('userstops_model');
+
+	}
+
+
 	public function index() {
+		$data = array();
+		$data['arrivals'] = $this->userstops_model->getArrivals();
+
 		$this->load->view('userstops');
 	}
 		
---
+++ application/models/userstops_model.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Userstops_model extends CI_model {
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function getArrivals() {
+
+		return array();
+	}
+
+}

a3a0e4c86a0f9a2ede41d5131571f63a14ba993f
Michal Hynek <hynek.michal@gmail.com>
2013-03-22 22:45:00 -0700

    Added DB data scripts.  Added controller that load's the mock page after the user logs in.

---
+++ application/controllers/stops.php
@@ -0,0 +1,12 @@
+<?php
+
+class Stops extends Restricted {
+	
+	public function index() {
+
+	}
+
+	public function find_stop($searchInput) {
+
+	}
+}
---
+++ application/views/userstops.php
@@ -0,0 +1,250 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include 'header.php'; ?>
+
+    <link href="<?php echo base_url(); ?>assets/css/busstop.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <?php include 'navbar_logged_in.php'; ?>
+
+    <div class="container-fluid">
+      <a class="btn btn-primary pull-left add-stop">Add Bus Stop</a>
+      <a class="btn btn-primary pull-right refresh">Update Now!</a>
+      <p class="pull-right"><strong>Last update at 8:17pm</strong></p>
+      <div class="row-fluid">
+        <div class="span12">
+          <div class="row-fluid">
+            <div class="span6 nextbus-pod">
+              <h3>W BROADWAY @ BURRARD ST</h3>
+              <div>
+                <strong>Eastbound</strong>
+                <a class="btn btn-primary btn-danger btn-small delete">Delete</a><br/>
+                <strong>Stop # 50235</strong>
+              </div>
+              <br/>
+              <table class="table busstop-table">
+                <th>Bus #</th>
+                <th>Destination</th>
+                <th class="rightCell">Arrives in</th>
+                <tr>
+                  <td>14</td>
+                  <td>DOWNTOWN</td>
+                  <td class="rightCell">1 min</td>
+                </tr>
+                <tr>
+                  <td>14</td>
+                  <td>TO GRANVILLE</td>
+                  <td class="rightCell">1 min</td>
+                </tr>
+                <tr>
+                  <td>9</td>
+                  <td>COMM'L-BDWAY STN</td>
+                  <td class="rightCell">2 min</td>
+                </tr>
+                <tr>
+                  <td>14</td>
+                  <td>DOWNTOWN</td>
+                  <td class="rightCell">12 min</td>
+                </tr>
+                <tr>
+                  <td>9</td>
+                  <td>BOUNDARY</td>
+                  <td class="rightCell">14 min</td>
+                </tr>
+              </table>
+
+              <small>* indicates scheduled departure time</small>
+              <div class="pagination pagination-mini pull-right">
+                <ul>
+                 <li><a href="#">&laquo;</a></li>
+                 <li><a href="#">1</a></li>
+                 <li><a href="#">2</a></li>
+                 <li><a href="#">3</a></li>
+                 <li><a href="#">4</a></li>
+                 <li><a href="#">&raquo;</a></li>
+               </ul>
+              </div>
+            </div><!--/span-->
+
+            <div class="span6 nextbus-pod">
+              <h3>W BROADWAY @ BURRARD ST</h3>
+              <div>
+                <strong>Eastbound</strong>
+                <a class="btn btn-primary btn-danger btn-small delete">Delete</a><br/>
+                <strong>Stop # 50235</strong>
+              </div>
+              <br/>
+              <table class="table busstop-table">
+                <th>Bus #</th>
+                <th>Destination</th>
+                <th class="rightCell">Arrives in</th>
+                <tr>
+                  <td>14</td>
+                  <td>DOWNTOWN</td>
+                  <td class="rightCell">1 min</td>
+                </tr>
+                <tr>
+                  <td>14</td>
+                  <td>TO GRANVILLE</td>
+                  <td class="rightCell">1 min</td>
+                </tr>
+                <tr>
+                  <td>9</td>
+                  <td>COMM'L-BDWAY STN</td>
+                  <td class="rightCell">2 min</td>
+                </tr>
+                <tr>
+                  <td>14</td>
+                  <td>DOWNTOWN</td>
+                  <td class="rightCell">12 min</td>
+                </tr>
+                <tr>
+                  <td>9</td>
+                  <td>BOUNDARY</td>
+                  <td class="rightCell">14 min</td>
+                </tr>
+
+              </table>
+
+              <small>* indicates scheduled departure time</small>
+              <div class="pagination pagination-mini pull-right">
+                <ul>
+                 <li><a href="#">&laquo;</a></li>
+                 <li><a href="#">1</a></li>
+                 <li><a href="#">2</a></li>
+                 <li><a href="#">3</a></li>
+                 <li><a href="#">4</a></li>
+                 <li><a href="#">&raquo;</a></li>
+               </ul>
+              </div>
+            </div><!--/span-->
+          </div><!--/row-->
+        </div><!--/span-->
+      </div><!--/row-->
+
+      <div class="row-fluid">
+        <div class="span12">
+          <div class="row-fluid">
+            <div class="span6 nextbus-pod">
+              <h3>W BROADWAY @ BURRARD ST</h3>
+              <div>
+                <strong>Eastbound</strong>
+                <a class="btn btn-primary btn-danger btn-small delete">Delete</a><br/>
+                <strong>Stop # 50235</strong>
+              </div>
+              <br/>
+              <table class="table busstop-table">
+                <th>Bus #</th>
+                <th>Destination</th>
+                <th class="rightCell">Arrives in</th>
+                <tr>
+                  <td>14</td>
+                  <td>DOWNTOWN</td>
+                  <td class="rightCell">1 min</td>
+                </tr>
+                <tr>
+                  <td>14</td>
+                  <td>TO GRANVILLE</td>
+                  <td class="rightCell">1 min</td>
+                </tr>
+                <tr>
+                  <td>9</td>
+                  <td>COMM'L-BDWAY STN</td>
+                  <td class="rightCell">2 min</td>
+                </tr>
+                <tr>
+                  <td>14</td>
+                  <td>DOWNTOWN</td>
+                  <td class="rightCell">12 min</td>
+                </tr>
+                <tr>
+                  <td>9</td>
+                  <td>BOUNDARY</td>
+                  <td class="rightCell">14 min</td>
+                </tr>
+              </table>
+
+              <small>* indicates scheduled departure time</small>
+              <div class="pagination pagination-mini pull-right">
+                <ul>
+                 <li><a href="#">&laquo;</a></li>
+                 <li><a href="#">1</a></li>
+                 <li><a href="#">2</a></li>
+                 <li><a href="#">3</a></li>
+                 <li><a href="#">4</a></li>
+                 <li><a href="#">&raquo;</a></li>
+               </ul>
+              </div>
+            </div><!--/span-->
+            <div class="span6 nextbus-pod">
+              <h3>W BROADWAY @ BURRARD ST</h3>
+              <div>
+                <strong>Eastbound</strong>
+                <a class="btn btn-primary btn-danger btn-small delete">Delete</a><br/>
+                <strong>Stop # 50235</strong>
+              </div>
+              <br/>
+              <table class="table busstop-table">
+                <th>Bus #</th>
+                <th>Destination</th>
+                <th class="rightCell">Arrives in</th>
+                <tr>
+                  <td>14</td>
+                  <td>DOWNTOWN</td>
+                  <td class="rightCell">1 min</td>
+                </tr>
+                <tr>
+                  <td>14</td>
+                  <td>TO GRANVILLE</td>
+                  <td class="rightCell">1 min</td>
+                </tr>
+                <tr>
+                  <td>9</td>
+                  <td>COMM'L-BDWAY STN</td>
+                  <td class="rightCell">2 min</td>
+                </tr>
+                <tr>
+                  <td>14</td>
+                  <td>DOWNTOWN</td>
+                  <td class="rightCell">12 min</td>
+                </tr>
+                <tr>
+                  <td>9</td>
+                  <td>BOUNDARY</td>
+                  <td class="rightCell">14 min</td>
+                </tr>
+              </table>
+
+              <small>* indicates scheduled departure time</small>
+              <div class="pagination pagination-mini pull-right">
+                <ul>
+                 <li><a href="#">&laquo;</a></li>
+                 <li><a href="#">1</a></li>
+                 <li><a href="#">2</a></li>
+                 <li><a href="#">3</a></li>
+                 <li><a href="#">4</a></li>
+                 <li><a href="#">&raquo;</a></li>
+               </ul>
+              </div>
+            </div><!--/span-->
+          </div><!--/row-->
+        </div><!--/span-->
+      </div><!--/row-->
+
+      <hr>
+
+      <footer>
+        <p><center>&copy; Michal Hynek and Colin Buckton 2013<center></p>
+      </footer>
+
+    </div><!--/.fluid-container-->
+
+
+    <script src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap-dropdown.js"></script>
+  </body>
+</html>

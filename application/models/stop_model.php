09bdeee27ac91dc85970800849d47326059a6b28
Michal Hynek <hynek.michal@gmail.com>
2013-03-24 09:55:39 -0700

    Implemented search by stop name

--- application/models/stop_model.php
+++ application/models/stop_model.php
@@ -9,17 +9,28 @@ class Stop_model extends CI_model {
 
 		if (is_numeric($searchInput)) {
 			try {
-				$stops[0] = $this->stop_model->get($searchInput);
+				$stops[0] = $this->getByCode($searchInput);
 			}
 			catch (StopNotFoundException $e) {
 				return array();	
 			}
 		}
+		else if (!empty($searchInput)) {
+			try {
+				$stops = $this->getByDescription($searchInput);
+			}
+			catch (StopNotFoundException $e) {
+				return array();	
+			}	
+		}
+		else {
+			return array();
+		}
 
 		return $stops;
 	}
 
-	public function get($code) {
+	public function getByCode($code) {
 		$query = $this->db->from($this->db->dbprefix('stops'))->where('code', $code)->get();
 
 		if ($query->num_rows() > 0) {
@@ -27,6 +38,17 @@ class Stop_model extends CI_model {
 		}
 		else {
 			throw new StopNotFoundException("Stop with code '$code' doesn't exist");	
+		}
+	}
+
+	public function getByDescription($description) {
+		$query = $this->db->from($this->db->dbprefix('stops'))->like('description', $description)->get();
+
+		if ($query->num_rows() > 0) {
+			return $query->result();
+		}
+		else {
+			throw new StopNotFoundException("Stop with name '$description' doesn't exist");	
 		}
 	}
 

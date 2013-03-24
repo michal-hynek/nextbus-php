481faacebee4e44faaa61cb05658baaddd8e4435
Michal Hynek <hynek.michal@gmail.com>
2013-03-23 18:41:06 -0700

    Implemented search for bus stops by code

--- application/controllers/stops.php
+++ application/controllers/stops.php
@@ -1,12 +1,28 @@
 <?php
 
+require_once 'restricted.php';
+
 class Stops extends Restricted {
 	
 	public function index() {
-
+		$this->load->view('add_stop');
 	}
 
-	public function find_stop($searchInput) {
+	public function find() {
+		$stops = array();
+		$this->load->model('stop_model');
 
+		$searchInput = $this->input->post('search_input');
+
+		$stops = $this->stop_model->find($searchInput);
+
+		$data = array();
+		$data['searchResult'] = $stops;
+
+		if (sizeof($stops) == 0) {
+			$data['errorMessage'] = "No bus stops match your criteria";
+		}
+
+		$this->load->view('add_stop', $data);
 	}
 }

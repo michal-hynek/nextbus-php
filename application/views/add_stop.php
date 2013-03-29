4780a6fe497268a132c2d59ac82cc1db0136587d
Michal Hynek <hynek.michal@gmail.com>
2013-03-28 17:51:46 -0700

    Fixed center alignment on 'Search Stop' page

--- application/views/add_stop.php
+++ application/views/add_stop.php
@@ -11,61 +11,67 @@
     <?php include 'navbar_logged_in.php' ?>
 
     <div class="container-fluid">
-      <div class="row">
+      <div class="row-fluid">
         <h1 class="title">Add Bus Stop</h1>
       </div>
 
       <form id="search_form" class="search-form" action="<?php echo base_url(); ?>index.php/stops/find" method="post">
-      <div class="row">
-        <div class="span7 offset4"> 
-          <input id="search_input" name="search_input" type="text" class="input-block-level" 
-                 data-provide="typeahead"
-                 autocomplete="off" placeholder="Type in bus stop # or location"
-                 value="<?php echo set_value('search_input'); ?>">
-        </div>
+      <div class="row-fluid">
+        <div class="span12">
+          <div class="span8 offset2"> 
+            <input id="search_input" name="search_input" type="text" class="input-block-level" 
+                   data-provide="typeahead"
+                   autocomplete="off" placeholder="Type in bus stop # or location"
+                   value="<?php echo set_value('search_input'); ?>">
+          </div>
 
-        <div class="span1">
-          <button class="btn btn-primary" type="submit">Search</button>
+          <div class="span1">
+            <button class="btn btn-primary" type="submit">Search</button>
+          </div>
         </div>
       </div><!--/row-->
       </form>
 
-      <div id="message-row" class="row">
+      <div class="row-fluid">
+        <div id="message-row" class="span12">
+        </div>
       </div>
 
-      <div class="row">
-        <div class="span12 offset2 search-result">
+      <div class="row-fluid">
+        <div class="span12">
+          <div class="span8 offset2 search-result">
 
-          <?php if (isset($searchResult) && sizeof($searchResult) > 0): ?>
-          <table class="table search-result-table">
-            <th>Stop #</th>
-            <th>Name</th>
-            <th>Description</th>
-            <?php if(isset($searchResult[0]->distance)): ?>
-            <th>Distance</th>
+            <?php if (isset($searchResult) && sizeof($searchResult) > 0): ?>
+            <table class="table search-result-table">
+              <th>Stop #</th>
+              <th>Name</th>
+              <th>Description</th>
+              <?php if(isset($searchResult[0]->distance)): ?>
+              <th>Distance</th>
+              <?php endif; ?>
+              <th></th>
+
+              <?php foreach ($searchResult as $stop): ?>
+                <tr>
+                  <td><?php echo $stop->code; ?></td>
+                  <td><?php echo $stop->name; ?></td>
+                  <td><?php echo $stop->description; ?></td>
+                  <?php if(isset($stop->distance)): ?>
+                  <td><?php echo round($stop->distance, 2) . " km"; ?></td>
+                  <?php endif; ?>
+                  <td class="rightCell">
+                    <a id="<?php echo $stop->code; ?>" class="btn btn-primary add_stop_button"
+                      href="#">
+                      Add
+                    </a>
+                  </td>
+                </tr>
+              <?php endforeach; ?>
+            </table>
             <?php endif; ?>
-            <th></th>
 
-            <?php foreach ($searchResult as $stop): ?>
-              <tr>
-                <td><?php echo $stop->code; ?></td>
-                <td><?php echo $stop->name; ?></td>
-                <td><?php echo $stop->description; ?></td>
-                <?php if(isset($stop->distance)): ?>
-                <td><?php echo round($stop->distance, 2) . " km"; ?></td>
-                <?php endif; ?>
-                <td class="rightCell">
-                  <a id="<?php echo $stop->code; ?>" class="btn btn-primary add_stop_button"
-                    href="#">
-                    Add
-                  </a>
-                </td>
-              </tr>
-            <?php endforeach; ?>
-          </table>
-          <?php endif; ?>
-
-        </div>
+          </div>
+      </div>
       </div>
 
       <hr>
@@ -108,7 +114,7 @@
       // add bus stop on 'add' button click
       var showErrorMessage = function(message) {
         $("#message-row").html(
-          '<div class="span8 offset4 alert alert-error">' +
+          '<div class="span8 offset2 alert alert-error">' +
             '<a href="#" class="close" data-dismiss="alert">&times;</a>' +
              message +
           '</div>');
@@ -116,7 +122,7 @@
 
       var showInfoMessage = function(message) {
         $("#message-row").html(
-          '<div class="span8 offset4 alert alert-info">' +
+          '<div class="span8 offset2 alert alert-info">' +
             '<a href="#" class="close" data-dismiss="alert">&times;</a>' +
              message +
           '</div>');
